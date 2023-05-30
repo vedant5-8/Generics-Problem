af92c384e40ec4a532a83187482bf00ac1aa31ba
Vedant <vedantnp200@gmail.com>
2023-05-30 12:17:38 +0530

    Generic class program is added

---
+++ Generics_Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Problem
+{
+    internal class Generics_Class<T> where T : IComparable<T>
+    {
+        // To compare two numbers are equal or not
+        public static bool AreEqual<T>(T value1, T value2)
+        {
+            return value1.Equals(value2);
+        }
+
+        // To find greater value from three value
+        public T CompareAll(T first,  T second, T third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0) 
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else
+            {
+                return third;
+            }
+
+            return default;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using Generics_Problem;
+
 namespace PracticeProject
 {
     class Program
@@ -6,6 +8,29 @@ namespace PracticeProject
         static void Main(string[] args)
         {
             Console.WriteLine("----Generics in C#----");
+
+            // Generic Class Demo
+
+            // bool equal = Generics_Class<int>.AreEqual(2, 2);
+            // bool equal = Generics_Class<string>.AreEqual("Vedant", "vedant");
+            bool equal = Generics_Class<float>.AreEqual(3.5f, 5.6f);
+
+            if (equal)
+            {
+                Console.WriteLine("Both Equal.");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal.");
+            }
+
+            Generics_Class<int> generics = new Generics_Class<int>();
+
+            int result = generics.CompareAll(1, 2, 3);
+
+            Console.WriteLine("The largest is " + result);
+
+
         }
     }
 }
