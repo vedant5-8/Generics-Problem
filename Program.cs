11fcd194c051a2b348b659f75e3919ea56076757
Vedant <vedantnp200@gmail.com>
2023-05-30 10:12:28 +0530

    Generics Program in C#

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace PracticeProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("----Generics i C#----");
+            Console.WriteLine("----Generics in C#----");
         }
     }
 }
